432f50b56940fce6863582eecb0ba6079e07d4f2
SilMontes <Silmontes2001@gmail.com>
2021-02-20 22:40:37 +0000

    Almost done

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,11 +1,14 @@
 // Your code here:
 
-    for (var i = 99; i >= 0; i--) {
-        if (i != 0){ 
-        console.log(i + " bottles of milk on the wall, " + i + " bottles of milk. Take one down and pass it around, " + i + " bottles of milk on the wall.")
-        console.log(i + " bottles of milk on the wall, "+ i + " bottles of milk. Take one down and pass it around " + i- 1 + " bottles of milk on the wall.")
-    }
-    else {
-        console.log("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
-    }
-    }
+    for (var i = 99; i >= 1; i=i-1) {
+       if (i !=0 && i!=1){
+            console.log(i + " bottles of milk on the wall, "+ i + " bottles of milk. Take one down and pass it around, " + parseInt(i-1)+ " bottles of milk on the wall.");
+       }
+      // else if(i==1){
+          else{ 
+           console.log(i+" bottle of milk on the wall, " +i+ " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall." + "</br>"+ "No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.");
+       }
+    //    else {
+    // //        console.log("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.");
+    //    }
+}
--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -6,7 +6,8 @@ var person = {
     lucky_numbers: [ 7, 11, 13, 17], //Array
     significant_other: person2   //Object, yes the same variable/object defined after
 };
-
+person.lucky_numbers[3]= 33;
+console.log(person.lucky_numbers[3]);
 var person2 = {
     name: "Jane",
     lastname: "Doe",
@@ -15,24 +16,34 @@ var person2 = {
     lucky_numbers: [ 2, 4, 6, 8],
     significant_other: person
 };
+var person3={
+    name: "Jimmy",
+    lastname: "Doe",
+    age: 13,
+    lucky_numbers: [1,2,3,4],
+    significant_other:null
+}
 
 var family = {
     lastname: "Doe",
-    members: [person, person2]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2, person3]   //Array of objects, don't forget to add Jimmy
 };
 
 
 function addAllFamilyLuckyNumbers(anArray){
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-
-  //To-Do: loop and add; consider nested loops
+ //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
+//Enter all your code here:
+for ( var i=0; i< anArray.length; i++){ 
+    //console.log(anArray[i]);
+      for (var x=0; x < anArray[i].lucky_numbers.length; x++){
+          sumOfAllLuckyNumbers += x;
+      }
+ }
   
   return sumOfAllLuckyNumbers;
 }
-
-//Enter all your code here:
-
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
